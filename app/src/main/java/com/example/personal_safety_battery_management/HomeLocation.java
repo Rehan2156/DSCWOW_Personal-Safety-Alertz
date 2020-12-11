754a1ba740162cca448bba1aa840b942fa2df47c
imrehan <rehanshaikh2156@gmail.com>
2020-12-11 17:19:26 +0530

    automatic sms and location difference implemented

---
+++ app/src/main/java/com/example/personal_safety_battery_management/HomeLocation.java
@@ -0,0 +1,85 @@
+package com.example.personal_safety_battery_management;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
+
+public class HomeLocation extends AppCompatActivity {
+    Button btPicker;
+    TextView textview;
+    int PLACE_PICKER_REQUEST = 1;
+    public static double homeLong=0,homeLat=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_location);
+
+        btPicker = findViewById(R.id.bt_picker);
+        textview = findViewById(R.id.text_view);
+
+        btPicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PlacePicker.IntentBuilder builder = new PlacePicker.IntentBuilder();
+                try {
+                    startActivityForResult(builder.build(HomeLocation.this),
+                            PLACE_PICKER_REQUEST);
+                } catch (GooglePlayServicesRepairableException e) {
+                    e.printStackTrace();
+                } catch (GooglePlayServicesNotAvailableException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode==PLACE_PICKER_REQUEST){
+            if(resultCode==RESULT_OK){
+                Place place = PlacePicker.getPlace(data, this);
+//                StringBuilder stringBuilder = new StringBuilder();
+                double latitude = (place.getLatLng().latitude);
+                double longitude = (place.getLatLng().longitude);
+                this.homeLat=latitude;
+                this.homeLong=longitude;
+//                stringBuilder.append("LATITUDE: ");
+//                stringBuilder.append(latitude);
+//                stringBuilder.append("\nLONGITUDE: ");
+//                stringBuilder.append(longitude);
+                textview.setText("LATITUDE IS "+latitude+". LONGITUDE IS "+longitude);
+                showMessageOKCancel("ARE YOU SURE?");
+
+
+            }
+        }
+
+    }
+    private void showMessageOKCancel(String message) {
+        new android.app.AlertDialog.Builder(HomeLocation.this)
+                .setMessage(message)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent myIntent = new Intent(getApplicationContext(), Dashboard.class);
+                        startActivityForResult(myIntent, 0);
+                    }
+                })
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
+    }
+}
---
+++ app/src/main/java/com/example/personal_safety_battery_management/SMS.java
@@ -0,0 +1,24 @@
+package com.example.personal_safety_battery_management;
+
+import android.telephony.SmsManager;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class SMS {
+
+    public static void sendSMS(ArrayList<String> a,String msg) {
+        for (String num : a) {
+            String phone_Num = num;
+            String send_msg = msg;
+            System.out.println("Phone number is "+phone_Num);
+            try {
+                SmsManager sms = SmsManager.getDefault(); // using android SmsManager
+                sms.sendTextMessage(phone_Num, null, send_msg, null, null); // adding number and text
+            } catch (Exception e) {
+                Toast.makeText(null, "Sms not Send", Toast.LENGTH_SHORT).show();
+                e.printStackTrace();
+            }
+        }
+    }
+}
